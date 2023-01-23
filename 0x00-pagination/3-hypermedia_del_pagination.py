1057f4bb947f3c39f9dedef71515bb511ffea2fb
HAMA Barhamou <hamabarhamou@gmail.com>
2023-01-23 09:44:04 +0100

    INitiation taske 3. Deletion-resilient hypermedia pagination

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""0x00. Pagination"""
+
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> (int, int):
+    """ function named index_range that takes two integer arguments
+        page and page_size.
+    """
+    if page == 1:
+        return (0, page_size)
+
+    return ((page-1) * page_size, (page - 1) * page_size + page_size)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        pass
