8ac6e26eb7583e6c43cc047bcdd3595d3e3fd685
HAMA Barhamou <hamabarhamou@gmail.com>
2023-01-27 19:48:12 +0100

    correction

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,69 +1,40 @@
 #!/usr/bin/env python3
-""" 0. Basic dictionary """
+"""Caching Algorithms"""
+
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """ BasicCache defines """
+    """Cache with LIFO (stack) structure"""
+    keys_order = []
 
     def __init__(self):
-        """ Initiliaze
-        """
+        """init from BaseCaching"""
         super().__init__()
-        self.nbObjet = 1
-        self.teteval = None
-        self.queudval = None
-        self.liste = None
-
-    def print_cache(self):
-        """ Print the cache
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)['data']))
 
     def put(self, key, item):
         """
-            - Must assign to the dictionary self.cache_data
-                the item value for the key key.
-            - If key or item is None, this method should not
-                do anything.
-            - If the number of items in self.cache_data is higher
-                that BaseCaching.MAX_ITEMS:
-                * you must discard the first item put in cache
-                    (FIFO algorithm)
-                * you must print DISCARD: with the key discarded and
-                    following by a new line
+        puts a new item to Cache key
+
+        if number of items reach a limit set
+        then the newest item is removed to make space
+        (Last in, First out)
         """
-
-        if key not in self.cache_data and item is not None:
-            if self.nbObjet > LIFOCache.MAX_ITEMS:
-                sauve = self.teteval
-                self.teteval = self.cache_data[self.teteval]['next']
-                print('DISCARD:', sauve)
-                self.cache_data.pop(sauve)
-
-            noeud = {'data': item, 'next': None}
-
-            if self.teteval is None:
-                self.queudval = key
-            else:
-                noeud['next'] = self.teteval
-                
-            self.teteval = key
-            self.cache_data[key] = noeud
-
-        self.nbObjet += 1
+        if not key or not item:
+            return
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.keys_order.append(key)
+            return
+        if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+            del_key = self.keys_order.pop()
+            del self.cache_data[del_key]
+            print(f"DISCARD: {del_key}")
+        self.cache_data[key] = item
+        self.keys_order.append(key)
 
     def get(self, key):
-        """
-            - Must return the value in self.cache_data linked to key.
-            - If key is None or if the key doesn’t exist in self.cache_data,
-                return None.
-        """
-
-        if key not in self.cache_data:
-            return None
-        if self.cache_data[key] is None:
+        """retrieves value from Cache key"""
+        if not key or key not in self.cache_data:
             return None
         return self.cache_data[key]
